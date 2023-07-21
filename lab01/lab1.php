4e5ce4d3722fe1092540bb5346c5b2003fdeaaef
Thang1011 <Thangnqbc@gmail.com>
2023-07-21 10:08:30 +0700

    lab1

--- lab01/lab1.php
+++ lab01/lab1.php
@@ -4,3 +4,31 @@ echo ("Hello <br>");
 echo "Hello World <br>";
 echo "Hello", "World";
 ?>
+
+//bài2
+
+<?
+$str = "hello string";
+$x = 200;
+$y = 44.6;
+echo "string is: $str <br/>";
+echo "integr is: $x <br/>";
+echo "float is: $y <br/>";
+?>
+// bài 3 
+<?php 
+$color="red";
+echo "My car is". $color ."<br>";
+echo "My house is". $COLOR ."<br>";
+echo "My boat is". $COLOR ."<br>";
+?>
+//Bài 1-2.php
+<?php 
+$s = "Hello\nWorld";
+echo $s;
+$s = 'It\'s\n';//"It's"
+echo $s;
+echo "\nHello<br>World";
+echo "\u{00C2A9}";//@(copyright sign)
+echo "\u{C2A9}";//@
+?>
