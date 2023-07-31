200fa8f2db4646a0318090d12b6ab9c34f890b09
Thang1011 <Thangnqbc@gmail.com>
2023-07-31 07:46:13 +0700

    Assignment

---
+++ Assignment/classes/user.php
@@ -0,0 +1,126 @@
+<?php
+$filepath = realpath(dirname(__FILE__));
+include_once($filepath . '/../lib/session.php');
+include_once($filepath . '/../lib/database.php');
+include_once($filepath . '/../lib/PHPMailer.php');
+include_once($filepath . '/../lib/SMTP.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+?>
+
+<?php
+/**
+ * 
+ */
+class user
+{
+	private $db;
+	public function __construct()
+	{
+		$this->db = new Database();
+	}
+
+	public function login($email, $password)
+	{
+		$query = "SELECT * FROM users WHERE email = '$email' AND password = '$password' LIMIT 1 ";
+		$result = $this->db->select($query);
+		if ($result) {
+			$value = $result->fetch_assoc();
+			Session::set('user', true);
+			Session::set('userId', $value['id']);
+			Session::set('role_id', $value['role_id']);
+			header("Location:index.php");
+		} else {
+			$alert = "Username or password is incorrect!";
+			return $alert;
+		}
+	}
+
+	public function insert($data)
+	{
+		$fullName = $data['fullName'];
+		$email = $data['email'];
+		$dob = $data['dob'];
+		$address = $data['address'];
+		$password = md5($data['password']);
+
+
+		$check_email = "SELECT * FROM users WHERE email='$email' LIMIT 1";
+		$result_check = $this->db->select($check_email);
+		if ($result_check) {
+			return 'Email đã tồn tại!';
+		} else {
+			// Genarate captcha
+			$captcha = rand(10000, 99999);
+
+			$query = "INSERT INTO users VALUES (NULL,'$email','$fullName','$dob','$password',2,1,'$address',0,'" . $captcha . "') ";
+			$result = $this->db->insert($query);
+			if ($result) {
+				// Send email
+				$mail = new PHPMailer();
+				$mail->IsSMTP();
+				$mail->Mailer = "smtp";
+
+				$mail->SMTPDebug  = 0;
+				$mail->SMTPAuth   = TRUE;
+				$mail->SMTPSecure = "tls";
+				$mail->Port       = 587;
+				$mail->Host       = "smtp.gmail.com";
+				$mail->Username   = "nguyenquocthang@gmail.com";
+				$mail->Password   = "1";
+
+				$mail->IsHTML(true);
+				$mail->CharSet = 'UTF-8';
+				$mail->AddAddress($email, "recipient-name");
+				$mail->SetFrom("nguyenquocthang@gmail.com", "Instrument Store");
+				$mail->Subject = "Confirm account email - Instruments Store";
+				$mail->Body = "<h3>Thank you for registering an account at StoreNows website</h3></br>This is your account verification code: " . $captcha . "";
+
+				$mail->Send();
+
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public function get()
+	{
+		$userId = Session::get('userId');
+		$query = "SELECT * FROM users WHERE id = '$userId' LIMIT 1";
+		$mysqli_result = $this->db->select($query);
+		if ($mysqli_result) {
+			$result = mysqli_fetch_all($this->db->select($query), MYSQLI_ASSOC)[0];
+			return $result;
+		}
+		return false;
+	}
+
+	public function getLastUserId()
+	{
+		$query = "SELECT * FROM users ORDER BY id DESC LIMIT 1";
+		$mysqli_result = $this->db->select($query);
+		if ($mysqli_result) {
+			$result = mysqli_fetch_all($this->db->select($query), MYSQLI_ASSOC)[0];
+			return $result;
+		}
+		return false;
+	}
+
+	public function confirm($userId, $captcha)
+	{
+		$query = "SELECT * FROM users WHERE id = '$userId' AND captcha = '$captcha' LIMIT 1";
+		$mysqli_result = $this->db->select($query);
+		if ($mysqli_result) {
+			// Update comfirmed
+			$sql = "UPDATE users SET isConfirmed = 1 WHERE id = $userId";
+			$update = $this->db->update($sql);
+			if ($update) {
+				return true;
+			}
+		}
+		return 'Verification code is not correct!';
+	}
+}
+?>
